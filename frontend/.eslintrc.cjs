4f557655ebf44322e846a6aa123029585570a007
Stanislav <55299328+Imsamaboy@users.noreply.github.com>
2026-01-10 17:13:41 +0300

    feat: add solution

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,15 +3,30 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  env: { browser: true, es2021: true, node: true },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    extraFileExtensions: [".vue"],
+  },
   extends: [
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"]
   },
-  env: {
-    "vue/setup-compiler-macros": true,
-  },
+  overrides: [
+    {
+      files: ["**/*.d.ts"],
+      rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+        "no-unused-vars": "off",
+      },
+    },
+  ],
 };
